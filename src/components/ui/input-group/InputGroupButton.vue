904d30c098147ff484fe56ad621aba897388dd3a
Yuri FA <yurik45gb@gmail.com>
2026-05-05 02:46:10 +0300

    feat: add UI form components
    Add shadcn-vue form components: button-group, checkbox, field,
    input-group, input, label, radio-group, select, separator, and textarea.

---
+++ src/components/ui/input-group/InputGroupButton.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import type { InputGroupButtonVariants } from '.'
+import type { ButtonVariants } from '@/components/ui/button'
+import { cn } from '@/shared/lib/utils'
+import { Button } from '@/components/ui/button'
+import { inputGroupButtonVariants } from '.'
+
+interface InputGroupButtonProps {
+  variant?: ButtonVariants['variant']
+  size?: InputGroupButtonVariants['size']
+  // eslint-disable-next-line vue/require-default-prop
+  class?: HTMLAttributes['class']
+}
+
+const props = withDefaults(defineProps<InputGroupButtonProps>(), {
+  size: 'xs',
+  variant: 'ghost',
+})
+</script>
+
+<template>
+  <Button
+    :data-size="props.size"
+    :variant="props.variant"
+    :class="cn(inputGroupButtonVariants({ size: props.size }), props.class)"
+  >
+    <slot />
+  </Button>
+</template>
