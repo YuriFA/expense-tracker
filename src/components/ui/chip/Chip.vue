3ee8271678fe71278676b72524bcdd3e919b5cfd
Yuri FA <yurik45gb@gmail.com>
2026-05-12 23:47:59 +0300

    refactor: replace Chip onRemove prop with emit event

--- src/components/ui/chip/Chip.vue
+++ src/components/ui/chip/Chip.vue
@@ -10,15 +10,18 @@ const props = defineProps<
   PrimitiveProps & {
     variant?: BadgeVariants['variant']
     class?: HTMLAttributes['class']
-    onRemove: () => void
   }
 >()
+
+const emit = defineEmits<{
+  (event: 'remove'): void
+}>()
 </script>
 
 <template>
   <Badge v-bind="props">
     <slot />
-    <Button variant="ghost" size="xs" class="p-1 size-5! rounded-full" @click="onRemove">
+    <Button variant="ghost" size="xs" class="p-1 size-5! rounded-full" @click="emit('remove')">
       <X />
     </Button>
   </Badge>
