742962889b4473a97830aec78ba86855d7223815
Yuri FA <yurik45gb@gmail.com>
2026-05-12 23:39:21 +0300

    feat: add vue-sonner for toast notifications

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import AppShell from '@/components/layout/AppShell.vue'
+import { Toaster } from '@/components/ui/sonner'
+import 'vue-sonner/style.css'
 </script>
 
 <template>
   <AppShell />
+  <Toaster />
 </template>
