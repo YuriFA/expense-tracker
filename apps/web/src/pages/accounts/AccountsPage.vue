a105c8c377d2a2f00d6836ad4463ba126b6cca25
Yuri FA <yurik45gb@gmail.com>
2026-05-13 14:59:16 +0300

    feat: add account creation with dialog

--- apps/web/src/pages/accounts/AccountsPage.vue
+++ apps/web/src/pages/accounts/AccountsPage.vue
@@ -5,7 +5,17 @@ import AccountCard from './AccountCard.vue'
 import { Card, CardContent } from '@/components/ui/card'
 import { useAccountBalances } from '@/composables/use-account-balances'
 import { useCurrencyFormatter } from '@/composables/use-currency-formatter'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { toast } from 'vue-sonner'
+import AddAccountForm from '@/features/add-account/AddAccountForm.vue'
 
 const { t } = useI18n()
 const accounts = useAccountsStore()
@@ -14,12 +24,34 @@ const { format } = useCurrencyFormatter()
 const totalBalanceFormatted = computed(() => {
   return format(totalBalance.value)
 })
+
+const dialogOpen = ref(false)
+const onSuccess = () => {
+  toast.success(t('addAccount.success'))
+  dialogOpen.value = false
+}
 </script>
 
 <template>
   <section>
-    <h1 class="text-2xl font-semibold">{{ t('pages.accounts') }}</h1>
-    <p class="text-muted-foreground text-sm">{{ t('accounts.description') }}</p>
+    <div class="flex items-center justify-between gap-4 flex-wrap">
+      <div class="flex-1">
+        <h1 class="text-2xl font-semibold">{{ t('pages.accounts') }}</h1>
+        <p class="text-muted-foreground text-sm">{{ t('accounts.description') }}</p>
+      </div>
+
+      <Dialog v-model:open="dialogOpen">
+        <DialogTrigger as-child>
+          <Button class="max-sm:w-full">{{ t('addAccount.new') }}</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{{ t('addAccount.newAccount') }}</DialogTitle>
+          </DialogHeader>
+          <AddAccountForm @success="onSuccess" />
+        </DialogContent>
+      </Dialog>
+    </div>
 
     <Card class="mt-4 bg-linear-to-br from-primary to-primary/80 text-primary-foreground">
       <CardContent>
@@ -30,8 +62,8 @@ const totalBalanceFormatted = computed(() => {
       </CardContent>
     </Card>
 
-    <ul class="mt-6 flex gap-4">
-      <li v-for="account in accounts.items" :key="account.id" class="flex-1">
+    <ul class="mt-6 grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4 flex-wrap">
+      <li v-for="account in accounts.items" :key="account.id">
         <AccountCard :account />
       </li>
     </ul>
