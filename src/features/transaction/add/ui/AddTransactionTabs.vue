b69662c78f0a790d0bd2b5bcc5902a1b3d8a50df
Yuri FA <yurik45gb@gmail.com>
2026-07-05 22:47:21 +0300

    refactor(transaction-add): persist last account ids in localStorage
    Replaces useLastCreatedTransaction (which fired a transactions query
    and gated forms on isLoading) with a small localStorage module that
    writes the last-used accountId(s) on successful submit and reads them
    synchronously at form init. Removes the dashboard mount/unmount loop
    driven by pinia-colada refreshes on the shared ['accounts'] query.

--- src/features/transaction/add/ui/AddTransactionTabs.vue
+++ src/features/transaction/add/ui/AddTransactionTabs.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import { useI18n } from 'vue-i18n'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/shared/ui/tabs'
 import { getTransactionsOptions } from '@/entities/transaction'
-import { Spinner } from '@/shared/ui/spinner'
-import { useLastCreatedTransaction } from '../model/use-transaction-form-data'
 import CashflowForm from './CashflowForm.vue'
 import TransferForm from './TransferForm.vue'
 
@@ -11,39 +8,24 @@ defineEmits<{
   success: []
 }>()
 
-const { t } = useI18n()
 const transactionTypes = getTransactionsOptions()
-const { lastCreatedCashflowTransaction, lastCreatedTransferTransaction, isReady } =
-  useLastCreatedTransaction()
 </script>
 
 <template>
-  <Tabs v-if="isReady" default-value="expense">
+  <Tabs default-value="expense">
     <TabsList class="w-full">
       <TabsTrigger v-for="item in transactionTypes" :key="item.value" :value="item.value">
         {{ item.label }}
       </TabsTrigger>
     </TabsList>
     <TabsContent value="expense">
-      <CashflowForm
-        type="expense"
-        :last-created-transaction="lastCreatedCashflowTransaction"
-        @success="$emit('success')"
-      />
+      <CashflowForm type="expense" @success="$emit('success')" />
     </TabsContent>
     <TabsContent value="income">
-      <CashflowForm
-        type="income"
-        :last-created-transaction="lastCreatedCashflowTransaction"
-        @success="$emit('success')"
-      />
+      <CashflowForm type="income" @success="$emit('success')" />
     </TabsContent>
     <TabsContent value="transfer">
-      <TransferForm
-        :last-created-transaction="lastCreatedTransferTransaction"
-        @success="$emit('success')"
-      />
+      <TransferForm @success="$emit('success')" />
     </TabsContent>
   </Tabs>
-  <Spinner v-else class="mx-auto my-6" />
 </template>
